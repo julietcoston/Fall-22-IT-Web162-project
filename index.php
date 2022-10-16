02c23e4ac914cdf8ecf28ca351ec0edf6d75f3fd
julietcoston <103077445+julietcoston@users.noreply.github.com>
2022-10-16 13:54:51 -0700

    Path/file

--- index.php
+++ index.php
@@ -31,28 +31,55 @@
 <section>
 
 <h2 class="pageID">Welcome</h2>
-
-<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
  <img src="images/desktop.jpg" class="desktop" alt="" />
  <img src="images/phone.jpg" class="phone" alt="" />
- <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>This  are my picture hope its okey</p>
  <p>Lorem Ipsum place holder text goes here for now</p>
  <p>Lorem Ipsum place holder text goes here for now</p>
  <p>Lorem Ipsum place holder text goes here for now</p>
 </section>
-<!-- END LEFT COL -->
+
+
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+
+ 
+ 
+  <footer>
+      <p><small>&copy; 2020-<?=date("Y")?> by 
+          <a href="contact.php">Sara Newman</a>, All Rights Reserved ~ 
+          <a id="html-checker" href="#">Check HTML</a> ~ 
+          <a id="css-checker" href="#">Check CSS</a></small>
+     </p>
+    </footer>
+<!-- END Footer --> 
+</main>
+
+
+<script>
+	//https://tinyurl.com/dynamic-html-checker
+	document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+            document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+
+	//manages mobile nav - from W3Schools
+	function myFunction() {
+    	    var x = document.getElementById("myTopnav");
+    	    if (x.className === "topnav") {
+        	         x.className += " responsive";
+    	    } else {
+        	         x.className = "topnav";
+    	   }
+	}   
+ </script>
+</body>
+</html>
 
 
     
-    <form>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="John"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe"><br><br>
-        <input type="submit" value="Submit">
-      </form>
-      
- 
    
 
       
