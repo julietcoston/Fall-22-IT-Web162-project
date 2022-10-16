c79c12c5cde4fd7da3892a9162fb5abafc139fc2
julietcoston <103077445+julietcoston@users.noreply.github.com>
2022-10-16 13:39:36 -0700

    HTLm introduction big/index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>My portal</title>
+<meta charset="UFT-8">
+<meta name="viewport" content="width=device-width" />
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/form.css" />
+</head>
+
+<body>
+<main class="wrapper">
+<header>
+<h1><a href="index.php"><i class="logo fa fa-home"></i> (My name is Juliet Coston here are my SCC IT162 Portal</a></h1>
+<nav>
+    <ul class="topnav" id="myTopnav">
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart/Layout</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contactme.php">Contact Sara</a></li>
+      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+</header>
+        
+<!-- START LEFT COL -->
+<section>
+
+<h2 class="pageID">Welcome</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="" />
+ <img src="images/phone.jpg" class="phone" alt="" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</section>
+<!-- END LEFT COL -->
+
+
+    
+    <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+      </form>
+      
+ 
+   
+
+      
+
+    
+</body>
+</html>
+
+  
+
+
+  
